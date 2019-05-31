bb68e020f789a6a258b19c6407573f06e2975f71
Thao Nguyen <thaonh@tohsoft.com>
2019-05-31 15:51:44 +0700

    Python refreshing

---
+++ Python/lists_tuples_sets.py
@@ -0,0 +1,20 @@
+# bad
+grade_one = 77
+grade_two = 80
+grade_three = 90
+
+print((grade_one + grade_two + grade_three) / 3)
+
+# list
+grades = [33, 55, 66, 77, 88]
+grades.append(199)
+print(sum(grades) / len(grades))
+
+# tuple
+numbers = (1, 2, 3, 4, 5) # can't append
+
+# set
+set_grades = {22, 44, 55, 66} # unique & unordered
+print(set_grades)
+
+# finish 12
---
+++ Python/variables_methods.py
@@ -0,0 +1,35 @@
+a = 5
+b = 10
+my_variable = 56
+any_variable_name = 20
+
+string_variable = "hello"
+single_quotes = 'string can have single quotes'
+
+print(my_variable)
+print(string_variable)
+print("hello, python!")
+print(123)
+
+var1 = 100
+var2 = 100
+num1 = 2
+num2 = 8
+num3 = num1 * num2
+print(num3)
+
+# single line comment
+"""
+test comment
+"""
+
+def my_print_method():
+    print("test method")
+
+my_print_method()
+
+def multiply_method(number_one, number_two):
+    return number_one * number_two
+
+print(multiply_method(3, 4))
+result = multiply_method(5, 5)
