ca1c9476dd996b9df1b0cc2d2a6a4d72d85fbd92
Thao Nguyen <thaonh@tohsoft.com>
2019-08-06 12:49:40 +0700

    Python refresh - Dictionary

---
+++ Python/dictionaries.py
@@ -0,0 +1,23 @@
+my_set = {1, 3, 5} #set
+my_dict = {'name': 'Jose', 'age': 90, 'grades': [13, 45, 66, 90]}
+another_dict = { 1: 15, 2: 75, 3: 150 }
+
+lottery_player = {
+    'name': 'Roft',
+    'numbers': (13, 45, 66, 23, 22)  #tuple
+}
+
+universities = [
+    {
+        'name': 'Oxford',
+        'location': 'UK'
+    },
+    {
+        'name': 'MIT',
+        'location': 'US'
+    }
+]
+
+sum(lottery_player['numbers'])
+
+lottery_player['name'] = 'John'
