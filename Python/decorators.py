431acbb43ece6946587a4cba7bd479711596d00b
Thao Nguyen <thaonh@tohsoft.com>
2019-08-15 13:18:44 +0700

    Advance decorator

--- Python/decorators.py
+++ Python/decorators.py
@@ -12,4 +12,25 @@ def my_decorator(func):
 def my_function():
     print("Im the function!")
 
-my_function()
+# my_function()
+
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_that_runs_func(*args, **kwargs):
+            print("In the decorator!")
+            if number == 56:
+                print("Not running the function!")
+            else:
+                func(*args, **kwargs)
+            print("After the decorator!")
+        return function_that_runs_func
+    return my_decorator
+
+@decorator_with_arguments(526)
+def my_function_too(x, y):
+    print("Hello")
+    print(x)
+    print(y)
+
+my_function_too(55, 66)
