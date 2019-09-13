ea2a2ebb528ca5546d5fdaa1921095da00692af1
Thao Nguyen <thaonh@tohsoft.com>
2019-09-13 08:04:24 +0700

    Creating models

---
+++ flasksqlalchemy/code/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.item import Item, ItemList
+from resources.user import UserRegister
+
+app = Flask(__name__)
+app.secret_key = "HMfxzSN2Mb"
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
